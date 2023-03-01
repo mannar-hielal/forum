e02766221fca8ebf0453b92bc3b4f73114e29cf2
mannar.hielal <adminwithu@gmail.com>
2023-03-01 22:37:37 +0100

    Implement add a post

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -4,6 +4,18 @@
         <h1>{{ thread.title }}</h1>
         <!-- Iterating through the posts of this specific thread-->
         <PostList :posts="posts"/>
+        <form @submit.prevent="addPost">
+          <div class="form-group">
+            <label for="postTextArea">Enter your Message:</label>
+            <textarea
+            class="form-control"
+            id="postTextArea"
+            rows="3"
+            v-model="newPostText">
+          </textarea>
+          <button type="submit" class="btn btn-primary mt-4">Submit</button>
+          </div>
+      </form>
       </div>
 </template>
 
@@ -19,7 +31,9 @@ export default {
   },
   data () {
     return {
-      thread: sourceData.threads[this.id]
+      thread: sourceData.threads[this.id],
+      users: sourceData.users,
+      newPostText: ''
     }
   },
   props: {
@@ -32,6 +46,26 @@ export default {
     posts () {
       const postIds = Object.values(this.thread.posts)
       return Object.values(sourceData.posts).filter(post => postIds.includes(post['.key']))
+    }
+  },
+  methods: {
+    addPost () {
+      const postId = `newPost${Math.random()}`
+      const post = {
+        '.key': postId,
+        publishedAt: Math.floor(Date.now() / 1000),
+        text: this.newPostText,
+        threadId: this.id,
+        userId: 'FsCDAk9w8NeXEceLV87arpsXjnQ2'
+      }
+      this.newPostText = ''
+
+      // adding the new post to posts, thread.post and
+      // in the users.posts array
+      // using the Vue.set to make them reactive
+      this.$set(sourceData.posts, postId, post)
+      this.$set(this.thread.posts, postId, postId)
+      this.$set(this.users[post.userId].posts, postId, postId)
     }
   }
 }
