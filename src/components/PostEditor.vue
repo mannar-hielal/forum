01a20a62ea1bb517c45901d314bf18e921a7d958
mannar.hielal <adminwithu@gmail.com>
2023-03-03 20:03:33 +0100

    Move post form to its own component
    and pass a save event to parent component (ThreadShow) using emit

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <label for="postTextArea">Enter your Message:</label>
+        <textarea
+        class="form-control"
+        id="postTextArea"
+        rows="3"
+        v-model="postText">
+      </textarea>
+      <button type="submit" class="btn btn-primary mt-4">Submit</button>
+      </div>
+      </form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'PostEditor',
+  data () {
+    return {
+      postText: ''
+    }
+  },
+  props: {
+    threadId: {
+      required: true
+    }
+  },
+  methods: {
+    save () {
+      const postId = `newPost${Math.random()}`
+      const post = {
+        '.key': postId,
+        publishedAt: Math.floor(Date.now() / 1000),
+        text: this.postText,
+        threadId: this.threadId,
+        userId: 'FsCDAk9w8NeXEceLV87arpsXjnQ2'
+      }
+      this.postText = ''
+      this.$emit('save', { post })
+    }
+  }
+}
+</script>
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -4,36 +4,29 @@
         <h1>{{ thread.title }}</h1>
         <!-- Iterating through the posts of this specific thread-->
         <PostList :posts="posts"/>
-        <form @submit.prevent="addPost">
-          <div class="form-group">
-            <label for="postTextArea">Enter your Message:</label>
-            <textarea
-            class="form-control"
-            id="postTextArea"
-            rows="3"
-            v-model="newPostText">
-          </textarea>
-          <button type="submit" class="btn btn-primary mt-4">Submit</button>
-          </div>
-      </form>
+        <PostEditor
+        :threadId= id
+        @save="addPost"
+        />
       </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
 import PostList from '@/components/PostList.vue'
+import PostEditor from '@/components/PostEditor.vue'
 console.log(sourceData)
 
 export default {
   name: 'ThreadShow',
   components: {
-    PostList
+    PostList,
+    PostEditor
   },
   data () {
     return {
       thread: sourceData.threads[this.id],
-      users: sourceData.users,
-      newPostText: ''
+      users: sourceData.users
     }
   },
   props: {
@@ -49,17 +42,8 @@ export default {
     }
   },
   methods: {
-    addPost () {
-      const postId = `newPost${Math.random()}`
-      const post = {
-        '.key': postId,
-        publishedAt: Math.floor(Date.now() / 1000),
-        text: this.newPostText,
-        threadId: this.id,
-        userId: 'FsCDAk9w8NeXEceLV87arpsXjnQ2'
-      }
-      this.newPostText = ''
-
+    addPost ({ post }) {
+      const postId = post['.key']
       // adding the new post to posts, thread.post and
       // in the users.posts array
       // using the Vue.set to make them reactive
