49f6dca8f30ab8807b7506245071c6e821d5ec1f
mannar.hielal <adminwithu@gmail.com>
2023-03-06 22:52:49 +0100

    Implement navbar component

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,21 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>-->
+    <TheNavbar/>
     <div class="container">
       <router-view/>
     </div>
   </div>
 </template>
+<script>
+import TheNavbar from './components/TheNavbar.vue'
 
+export default {
+  components: {
+    TheNavbar
+  }
+}
+</script>
 <style>
 @import './assets/styles.css';
 @import 'bootstrap/dist/css/bootstrap.min.css';
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,26 @@
+<template>
+<header class="header" id="header">
+  <router-link
+  :to="{ name: 'Home'}"
+  class="logo">
+  <img src="../assets/img/svg/vueschool-logo.svg">
+  </router-link>
+
+  <!-- use .navbar-open to open nav -->
+  <nav class="navbar">
+    <ul>
+      <li class="navbar-user">
+          <a href="#">
+              <img class="avatar-small" src="../assets/img/jpg/mannar-hielal-400X400.jpg" alt="">
+              <span>
+                  Mannar Hielal
+                  <img src="../assets/img/svg/arrow-profile.svg">
+              </span>
+          </a>
+      </li>
+    </ul>
+  </nav>
+</header>
+</template>
+<script>
+</script>
