808ce43f3619ec93f5d0f8f0196ed1e8c7fb738b
mannar.hielal <adminwithu@gmail.com>
2023-03-17 15:20:09 +0100

    Style the categoryListItem

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="category-list">
     <CategoryListItem v-for="category in categories"
     :category="category"
     :key="category['.key']"/>
--- src/components/CategoryListItem.vue
+++ src/components/CategoryListItem.vue
@@ -1,18 +1,20 @@
 <template>
   <!-- Display the category name-->
-  <div class="forum-list">
-    <h2 class="list-title">
-        <router-link
-          :to="{
-            name: 'Category',
-            params: { id: category['.key'] },
-          }"
-          class="text-xlarge"
-        >{{ category.name }}
-        </router-link>
+  <div class="category-list-item mb-12 bg-light rounded--bottom">
+    <h2 :class="`category-title p-4 h5 rounded--bottom m-0 bg--gradient-${index}`">
+      <router-link
+        :to="{
+          name: 'Category',
+          params: { id: category['.key'] },
+        }"
+        class="text-white"
+      >{{ category.name }}
+      </router-link>
     </h2>
-    <!-- Iterating through the forums of this category-->
-    <ForumList :forums="forums"/>
+    <div class="category-body p-4 rounded--bottom">
+      <!-- Iterating through the forums of this category-->
+      <ForumList :forums="forums"/>
+    </div>
   </div>
 </template>
 
@@ -38,6 +40,11 @@ export default {
     forums () {
       const forumIds = Object.values(this.category.forums)
       return Object.values(this.$store.state.forums).filter(forum => forumIds.includes(forum['.key']))
+    },
+    index () {
+      // get the index and use to generate random color
+      const index = this.category['.key']
+      return Object.keys(this.$store.state.categories).indexOf(index) + 1
     }
   }
 }
