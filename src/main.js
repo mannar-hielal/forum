d8329dc14aa0dce71a6e4e01c059cfca4798d5f4
mannar.hielal <adminwithu@gmail.com>
2023-03-20 19:51:52 +0100

    Install fontawesome and use it

--- src/main.js
+++ src/main.js
@@ -4,10 +4,23 @@ import router from './router'
 import store from './store'
 import AppDate from '@/components/AppDate.vue'
 import 'bootstrap/dist/js/bootstrap.js'
-import '@/assets/styles.scss'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faFaceSmile } from '@fortawesome/free-regular-svg-icons'
+
+/* add icons to the library */
+library.add(faFaceSmile)
 
 Vue.config.productionTip = false
 Vue.component('AppDate', AppDate)
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
