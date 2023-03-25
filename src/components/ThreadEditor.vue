8b04bb357e44bb2b46abdb77aca27b37dc01e4c3
mannar.hielal <adminwithu@gmail.com>
2023-03-25 21:58:43 +0100

    Move thread editor to its own component

---
+++ src/components/ThreadEditor.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <form>
+        <div class="mb-4">
+          <label for="thread_title">Title:</label>
+          <input v-model="title" type="text" id="thread_title" class="form-control" name="title">
+        </div>
+
+        <div class="mb-4">
+          <label for="thread_content">Content:</label>
+          <textarea v-model="text" id="thread_content" class="form-control" name="content" rows="8" cols="140"></textarea>
+        </div>
+
+        <div class="actions d-flex justify-content-end">
+            <button @click.prevent="cancel" class="btn btn-secondary me-2">Cancel</button>
+            <button @click.prevent="save" type="submit" class="btn btn-primary">Publish</button>
+        </div>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ThreadEditor',
+  data () {
+    return {
+      title: '',
+      text: ''
+    }
+  },
+  methods: {
+    save () {
+      this.$emit('save', { title: this.title, text: this.text })
+    },
+    cancel () {
+      this.$emit('cancel')
+    }
+  }
+}
+</script>
--- src/pages/PageThreadCreate.vue
+++ src/pages/PageThreadCreate.vue
@@ -1,32 +1,21 @@
 <template>
   <div>
     <h1>Create new thread in {{ forum.name }}</h1>
-    <form>
-        <div class="mb-4">
-          <label for="thread_title">Title:</label>
-          <input v-model="title" type="text" id="thread_title" class="form-control" name="title">
-        </div>
-
-        <div class="mb-4">
-          <label for="thread_content">Content:</label>
-          <textarea v-model="text" id="thread_content" class="form-control" name="content" rows="8" cols="140"></textarea>
-        </div>
-
-        <div class="actions d-flex justify-content-end">
-            <button @click.prevent="cancel" class="btn btn-secondary me-2">Cancel</button>
-            <button @click.prevent="save" type="submit" class="btn btn-primary">Publish</button>
-        </div>
-    </form>
+    <ThreadEditor
+    @save="save"
+    @cancel="cancel"/>
   </div>
 </template>
 <script>
+import ThreadEditor from '@/components/ThreadEditor.vue'
+
 export default {
   name: 'ThreadCreate',
+  components: {
+    ThreadEditor
+  },
   data () {
-    return {
-      title: '',
-      text: ''
-    }
+    return {}
   },
   props: {
     forumId: {
@@ -41,11 +30,11 @@ export default {
   },
   methods: {
     // we make creatThread promisified because we want it to return the created thread
-    save () {
+    save ({ title, text }) {
       this.$store.dispatch('createThread', {
         forumId: this.forum['.key'],
-        title: this.title,
-        text: this.text
+        title: title,
+        text: text
       }).then(thread => this.$router.push({ name: 'ThreadShow', params: { id: thread['.key'] } }))
     },
     cancel () {
